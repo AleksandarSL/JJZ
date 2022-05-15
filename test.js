0a851d03ffc4030ee3a01ad87bdc01780da9dfe5
AleksandarSL <101926785+AleksandarSL@users.noreply.github.com>
2022-05-15 18:16:10 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+function testfunction1() {
+    document.getElementById("test").innerHTML = "Hello Button";
+    }
+    function testfunction2() {
+    document.getElementById("tes").innerHTML = "Hello Div";
+    }
+
+    var x = 5
+
+    function myTest(){
+        console.log("Testiram x ="+ x)}
+        function foo(){ let x = 3;}
+
+      var content = document.getElementsById("content")
+      var button = document.getElementsById("Click")
+
+      function Dugme(){
+        var ime = "Aleksandar";
+        var prezime = " Obuhov";
+        var zajedno = ime + prezime;     
+        document.getElementById("test").innerHTML = zajedno;
+    }  
+   
+    
