4e3cd521d2c110aefa0f7dcbb355986942edbf5c
AleksandarSL <101926785+AleksandarSL@users.noreply.github.com>
2022-06-03 15:02:18 +0200

    Add files via upload

--- Site/test.js
+++ Site/test.js
@@ -1,8 +1,8 @@
 function testfunction1() {
-    document.getElementById("test").innerHTML = "Hello Button";
+    document.getElementById("test").innerHTML = "Hello";
     }
     function testfunction2() {
-    document.getElementById("tes").innerHTML = "Hello Div";
+    document.getElementById("tes").innerHTML = "Hello";
     }
 
 
@@ -20,7 +20,8 @@ function testfunction1() {
         document.getElementById("test").innerHTML = zajedno;
     } */ 
 
-   let btn = document.getElementsById("click")
+   /*
+    let btn = document.getElementsById("click")
     function Poruka(){
      let mess = prompt("Sta vas interesuje?");
      localStorage.setItem('poruka', mess);
@@ -34,11 +35,11 @@ function testfunction1() {
    if(!localStorage.getItem('mess')) {saveMess();}
    el.textContent = localStorage.getItem('mess')
 
-
-   let projekti {
+   let projekti {}
      name = "Proj",
      Projekat : [
-       { p1 = "3D",
+       { 
+         p1 = "3D",
          podaci = "text"
        }
       ]
@@ -50,4 +51,42 @@ function testfunction1() {
      p1El.textContent = obj['naziv'];
      
    }
-    
+   */
+ let projektii = {
+   Projekat1 : [
+     {
+    name: "3D Landscape",
+    description: "Just trying to figure stuff out.",
+    program:[ { naziv:"Blender", verzija:"2.9.8", format_fajla:"FBX"} ],
+    cena: "200e"
+     }
+   ],
+   Projekat2 : [
+    {
+   name: "Photo edit",
+   description: "Just trying to figure stuff out.",
+   program: [ { naziv:"Photoshop", verzija:"CC-2020", format_fajla:"JPG"} ],
+   cena: "100e"
+    }
+  ],
+  Projekat3 : [
+    {
+   name: "Website design",
+   description: "Just trying to figure stuff out.",
+   program : [ { naziv:"Illustrator", verzija:"CC", format_fajla:"EPS"} ],
+   cena: "500e"
+    }
+  ]
+  };
+  
+  console.log(projektii); 
+
+
+
+  const url = 'Base.json'; // OVDE JAVLJA GRESKU
+  async function getData(){
+    const response = await fetch(url);
+    const data = await response.json;
+    console.log(data);
+  }
+  getData();
